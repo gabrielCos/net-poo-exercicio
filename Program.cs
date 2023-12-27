12d6a55e63ff9a00b379c1aace930884fda48b86
Gabriel Costa <chris-19121@hotmail.com>
2023-12-27 14:14:39 -0300

    Conclusão do exercício

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia cel = new Nokia("85999628127", "A14", "1191919981919", 128);
+
+cel.InstalarAplicativo("Telegram");
+
+Iphone cel2 = new Iphone("96954123457", "I15", "1818191999199", 264);
+
+cel2.InstalarAplicativo("Stremio");
